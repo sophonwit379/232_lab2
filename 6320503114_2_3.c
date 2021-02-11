9e9ebb74a4092dd6ff6ba47714a109cd78d4c4be
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-02-11 17:32:35 +0700

    fix

--- 6320503114_2_3.c
+++ 6320503114_2_3.c
@@ -1,11 +1,27 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c;
-    int num[3];
+    int a,b,c,i,max,min;
     char ch[3];
     scanf("%d %d %d",&a,&b,&c);
+    int num[3]={a,b,c};
+    if(a>b && a>c)
+        max=a;
+    else if(b>a && a>c)
+        max=b;
+    else
+        max=c;
+    if(a<b && a<c)
+        min=a;
+    else if(b<a && b<c)
+        min=b;
+    else
+        min=c;
     scanf("%s",ch);
+    for(i=0;i<3;i++)
+    {
+
+    }
 
 
 
