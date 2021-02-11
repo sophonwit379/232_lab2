afb131847f4570c5de1064e68946178547682b46
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-02-11 15:05:37 +0700

    if grade

--- 6320503114_2_1.c
+++ 6320503114_2_1.c
@@ -1,12 +1,27 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c;
+    int a,b,c,r;
     scanf("%d",&a);
     scanf("%d",&b);
     scanf("%d",&c);
-
-
+    r=a+b+c;
+    if(80<=r && r<=100)
+        printf("A");
+    else if(75<=r && r<=79)
+        printf("B+");
+    else if(70<=r && r<=74)
+        printf("B");
+    else if(65<=r && r<=69)
+        printf("C+");
+    else if(60<=r && r<=64)
+        printf("C");
+    else if(55<=r && r<=59)
+        printf("D+");
+    else if(50<=r && r<=54)
+        printf("D");
+    else if(0<=r && r<=49)
+        printf("F");
 
 
 }
