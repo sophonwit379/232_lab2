89abfb3c04650da5f38a6236b6693002b9f7f874
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-02-11 15:54:59 +0700

    fix

--- 6320503114_2_2.c
+++ 6320503114_2_2.c
@@ -2,23 +2,37 @@
 #include<math.h>
 int main()
 {
-    float n,r,r2;
+    float n,r,r2,r3,r4,s;
     char pro;
     scanf("%c",&pro);
     scanf("%f",&n);
-    if(pro = 'A')
+    if(pro == 'A')
     {
-        r=n-200;
-        if(r<0)
-            printf("199.00");
-        else
+        s=199;
+        if(n>199)
+        {
+            r=n-199;
+            r2=(r*100)-(floor(r)*100);
+            r3=(r*100)-r2;
+            r+=r3;
+            r4=(r2*3)/60.0;
+            r+=r4;
+        printf("%0.2f",s);
+        }
     }
-    else if(pro = 'B')
+    else if(pro == 'B')
     {
-        r=n-400;
-        if(r<0)
-            printf("299.00");
-        else
+        s=299;
+        if(n>299)
+        {
+            r=n-299;
+            r2=(r*100)-(floor(r)*100);
+            r3=(r*100)-r2;
+            r+=r3;
+            r4=(r2*2)/60.0;
+            r+=r4;
+        }
+        printf("%0.2f",s);
     }
 
 
