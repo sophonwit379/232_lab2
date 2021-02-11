375485358f15621735778f2b2f4f03d759bd916e
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-02-11 15:18:23 +0700

    if

--- 6320503114_2_2.c
+++ 6320503114_2_2.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-    float n;
+    float n,r,r2;
     char pro;
     scanf("%c",&pro);
     scanf("%f",&n);
-    if(pro = 'A');
-
+    if(pro = 'A')
+    {
+        r=abs(200-n);
+    }
     else if(pro = 'B')
-
-
-
+    {
+        r=abs(400-n);
+    }
 
 
 }
