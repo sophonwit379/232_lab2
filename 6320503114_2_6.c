a6b7a6befde4ae856e2bc257f880a8c98c89b7de
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-02-11 16:23:25 +0700

    while switch

--- 6320503114_2_6.c
+++ 6320503114_2_6.c
@@ -1,8 +1,24 @@
 #include<stdio.h>
 int main()
 {
+    int num,tmp;
+    scanf("%d",&num);
+    i=num;
+    while(i>0)
+    {
+        tmp=i;
+        tmp%=10;
+        switch(tmp)
+        {
+            case 0:
+            case 1:
+            case 2:
 
 
+        }
+        i/=10;
+
+    }
 
 
 }
