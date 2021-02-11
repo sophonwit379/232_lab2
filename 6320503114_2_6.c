0e2991aa01f56aa8669e14f965c68757d30f410a
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-02-11 17:04:55 +0700

    Fix

--- 6320503114_2_6.c
+++ 6320503114_2_6.c
@@ -1,55 +1,48 @@
 #include<stdio.h>
 int main()
 {
-    int num,tmp,i,tmp2,rev=0;
-    scanf("%d",&num);
-    while(num!=0)
+    int i;
+    char num[8];
+    scanf("%s",num);
+    for(i=0;i<num[i];i++)
     {
-        tmp2=num%10;
-        rev=(rev*10)+tmp2;
-        num/=10;
-    }
-    i=rev;
-    while(i>0)
-    {
-        tmp=i;
-        tmp%=10;
-        switch(tmp)
+        switch(num[i])
         {
-            case 0:
+            case '0':
                 printf("Zero");
                 break;
-            case 1:
+            case '1':
                 printf("One");
                 break;
-            case 2:
+            case '2':
                 printf("Two");
                 break;
-            case 3:
+            case '3':
                 printf("Three");
                 break;
-            case 4:
+            case '4':
                 printf("Four");
                 break;
-            case 5:
+            case '5':
                 printf("Five");
                 break;
-            case 6:
+            case '6':
                 printf("Six");
                 break;
-            case 7:
+            case '7':
                 printf("Seven");
                 break;
-            case 8:
+            case '8':
                 printf("Eight");
                 break;
-            case 9:
+            case '9':
                 printf("Nine");
                 break;
         }
-         printf("-");
-        i/=10;
+        if(i<num[i+1])
+        {
+            printf("-");
+        }
     }
-
-
 }
+
