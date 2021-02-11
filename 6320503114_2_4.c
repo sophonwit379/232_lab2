1ce2c66ebe83f4d76d23f27feb1f983e686fbaef
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-02-11 17:58:55 +0700

    com

--- 6320503114_2_4.c
+++ 6320503114_2_4.c
@@ -1,13 +1,51 @@
 #include<stdio.h>
-int main
+int main()
 {
-    int num,a,b=92;
+    int num,b=92;
     char ch;
     scanf("%c",&ch);
     scanf("%d",&num);
-
-
+    if(num%10==5)
+    {
+        printf("%c",b);
+    }
+    if(num%2==1)
+    {
+        if(ch>=65&&ch<=73)
+        {
+            printf("(^_^)");
+        }
+        if(ch>=74&&ch<=82)
+        {
+            printf("(o)");
+        }
+        if (ch>=83&&ch<=90)
+        {
+            printf("(T_T)");
+        }
+    }
+    else
+    {
+        if(ch>=65&&ch<=73)
+            {
+                printf("{@_@}");
+            }
+        if(ch>=74&&ch<=82)
+            {
+                printf("{v}");
+            }
+        if (ch>=83&&ch<=90)
+            {
+                printf("{x_x}");
+            }
+    }
+    if(num%10==5)
+    {
+        printf("/");
+    }
 }
+
+
 
 
 
